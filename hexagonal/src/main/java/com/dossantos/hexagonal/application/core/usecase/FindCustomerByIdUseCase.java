437a336dd1eb00556ce35de2f94e58290c3b9b46
theuslinor <matheusdossantos3157@gmail.com>
2024-08-05 17:10:06 -0300

    feat: adicionando o findbyid

--- hexagonal/src/main/java/com/dossantos/hexagonal/application/core/usecase/FindCustomerByIdUseCase.java
+++ hexagonal/src/main/java/com/dossantos/hexagonal/application/core/usecase/FindCustomerByIdUseCase.java
@@ -1,10 +1,19 @@
 package com.dossantos.hexagonal.application.core.usecase;
 
 import com.dossantos.hexagonal.application.core.domain.Customer;
+import com.dossantos.hexagonal.application.ports.out.FindCustomerByIdOutputPort;
+
 
 public class FindCustomerByIdUseCase {
 
+    private final FindCustomerByIdOutputPort findCustomerByIdOutputPort;
+
+    public FindCustomerByIdUseCase(FindCustomerByIdOutputPort findCustomerByIdOutputPort) {
+        this.findCustomerByIdOutputPort = findCustomerByIdOutputPort;
+    }
+
     public Customer find(String id) {
-        
+        return findCustomerByIdOutputPort.find(id)
+                .orElseThrow(() -> new RuntimeException("Customer not found"));
     }
 }
---
+++ hexagonal/src/main/java/com/dossantos/hexagonal/application/ports/out/FindCustomerByIdOutputPort.java
@@ -0,0 +1,11 @@
+package com.dossantos.hexagonal.application.ports.out;
+
+import com.dossantos.hexagonal.application.core.domain.Customer;
+
+import java.util.Optional;
+
+public interface FindCustomerByIdOutputPort {
+
+    Optional<Customer> find(String id);
+
+}
